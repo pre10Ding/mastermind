4868930e613296633b06f7ffe707533e8ff7407a
Cheng Shao <cheng.shao38@gmail.com>
2020-12-19 16:53:19 -0500

    Move input validation method from Mastermind class to Player class.

--- player.rb
+++ player.rb
@@ -12,4 +12,17 @@ class Player
   def prompt
     gets.chomp
   end
+
+  def validate_input(question_to_display, valid_characters = %w[Y N], length = 1)
+    input = false
+    loop do
+      puts question_to_display
+      input = prompt.upcase
+      # subtracting sets leaves an empty if all eles in the first set exist in 2nd
+      break if input.length == length && (input.chars - valid_characters).empty?
+
+      puts 'Invalid input.'
+    end
+    input
+  end
 end
